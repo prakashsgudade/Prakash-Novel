8e124539f916e83192aa0278c00c785e2d23eeb4
prakashsgudade <prakashsgudade@gmail.com>
2026-01-28 18:00:35 +0530

    Update novels.js

--- js/novels.js
+++ js/novels.js
@@ -1,1 +1,7 @@
-
+window.addEventListener("load", () => {
+  document.querySelectorAll(".fade").forEach((el, i) => {
+    setTimeout(() => {
+      el.classList.add("show");
+    }, 200 + i*100);
+  });
+});
