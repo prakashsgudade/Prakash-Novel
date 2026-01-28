c49934100f1d0e9d32861c4b2f89f56c3ee1a3db
prakashsgudade <prakashsgudade@gmail.com>
2026-01-28 10:53:50 +0530

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const fadeElements = document.querySelectorAll(".fade");
 
+  fadeElements.forEach((el, index) => {
+    setTimeout(() => {
+      el.classList.add("show");
+    }, index * 250);
+  });
+});
