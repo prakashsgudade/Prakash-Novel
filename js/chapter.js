38d119de6a6fc8d353c76a9bc4e418f3402621d9
prakashsgudade <prakashsgudade@gmail.com>
2026-01-29 07:20:22 +0530

    Update chapter.js

--- js/chapter.js
+++ js/chapter.js
@@ -1,1 +1,20 @@
+let size = 18;
+const content = document.querySelector(".content");
 
+function fontPlus(){
+  size++;
+  content.style.fontSize = size + "px";
+}
+
+function fontMinus(){
+  size--;
+  content.style.fontSize = size + "px";
+}
+
+function darkMode(){
+  document.body.classList.remove("sepia");
+}
+
+function sepiaMode(){
+  document.body.classList.add("sepia");
+}
