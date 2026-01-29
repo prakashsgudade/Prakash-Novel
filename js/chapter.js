b2d471871abd7900134624c1f99ff9c25a184a13
prakashsgudade <prakashsgudade@gmail.com>
2026-01-29 08:35:30 +0530

    Update chapter.js

--- js/chapter.js
+++ js/chapter.js
@@ -1,15 +1,26 @@
-let fontSize = 18;
+let isDark = true;
 
-function toggleMode(){
-  document.body.classList.toggle("sepia");
+function toggleMode() {
+  if(isDark){
+    document.body.classList.remove('dark-mode');
+    document.body.classList.add('sepia-mode');
+  } else {
+    document.body.classList.remove('sepia-mode');
+    document.body.classList.add('dark-mode');
+  }
+  isDark = !isDark;
 }
 
-function increaseFont(){
-  fontSize++;
-  document.querySelector(".content").style.fontSize = fontSize + "px";
+function increaseFont() {
+  let content = document.querySelector('.content');
+  let style = window.getComputedStyle(content, null).getPropertyValue('font-size');
+  let currentSize = parseFloat(style);
+  content.style.fontSize = (currentSize + 2) + 'px';
 }
 
-function decreaseFont(){
-  fontSize--;
-  document.querySelector(".content").style.fontSize = fontSize + "px";
+function decreaseFont() {
+  let content = document.querySelector('.content');
+  let style = window.getComputedStyle(content, null).getPropertyValue('font-size');
+  let currentSize = parseFloat(style);
+  content.style.fontSize = (currentSize - 2) + 'px';
 }
