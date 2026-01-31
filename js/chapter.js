e1577882b510b15162ab8011910e33014bedbb0b
prakashsgudade <prakashsgudade@gmail.com>
2026-01-31 11:10:45 +0530

    Update chapter.js

--- js/chapter.js
+++ js/chapter.js
@@ -1,5 +1,6 @@
 const pages = document.querySelectorAll(".page");
 const counter = document.getElementById("pageCounter");
+const totalPages = pages.length;
 
 window.addEventListener("scroll", () => {
   let current = 1;
@@ -8,10 +9,10 @@ window.addEventListener("scroll", () => {
       current = i + 1;
     }
   });
-  counter.innerText = "Page " + current;
+  counter.innerText = "Page " + current + " / " + totalPages;
 });
 
-/* DARK MODE FUNCTION */
+/* DARK MODE */
 function toggleDark() {
   document.body.classList.toggle("dark");
 }
