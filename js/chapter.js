7237f5955828d0863bd34c5dfcc17685e03d83c2
prakashsgudade <prakashsgudade@gmail.com>
2026-01-31 10:51:04 +0530

    Update chapter.js

--- js/chapter.js
+++ js/chapter.js
@@ -1,8 +1,18 @@
+let currentPage = 1;
 const pages = document.querySelectorAll(".page");
+
 document.getElementById("totalPages").innerText = pages.length;
 
-let dark = true;
-document.getElementById("themeBtn").onclick = () => {
-  document.body.style.background = dark ? "#111" : "#c9b08a";
-  dark = !dark;
-};
+window.addEventListener("scroll", () => {
+  pages.forEach((page, index) => {
+    const rect = page.getBoundingClientRect();
+    if(rect.top < window.innerHeight/2 && rect.bottom > window.innerHeight/2){
+      currentPage = index + 1;
+      document.getElementById("pageNum").innerText = currentPage;
+    }
+  });
+});
+
+function toggleTheme(){
+  document.body.classList.toggle("light");
+}
