f6e184fb3750752cdf7c01aa5a891c09fa3652d5
prakashsgudade <prakashsgudade@gmail.com>
2026-01-31 13:20:12 +0530

    Update chapter.js

--- js/chapter.js
+++ js/chapter.js
@@ -1,18 +1,19 @@
-const pages = document.querySelectorAll(".page");
-const counter = document.getElementById("pageCounter");
-const totalPages = pages.length;
+// THEME TOGGLE
+const themeBtn = document.getElementById("themeToggle");
 
-window.addEventListener("scroll", () => {
-  let current = 1;
-  pages.forEach((page, i) => {
-    if (page.getBoundingClientRect().top < window.innerHeight / 2) {
-      current = i + 1;
-    }
-  });
-  counter.innerText = "Page " + current + " / " + totalPages;
+themeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    themeBtn.textContent = "☀️";
+  } else {
+    themeBtn.textContent = "🌙";
+  }
 });
 
-/* DARK MODE */
-function toggleDark() {
-  document.body.classList.toggle("dark");
-}
+// LIKE BUTTON
+const likeBtn = document.getElementById("likeBtn");
+
+likeBtn.addEventListener("click", () => {
+  likeBtn.textContent = "💖";
+});
